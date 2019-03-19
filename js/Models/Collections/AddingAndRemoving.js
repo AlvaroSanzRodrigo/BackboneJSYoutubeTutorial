25aca1889a3333090901f5599fb05784046456c9
AlvaroSanzRodrigoo <alvaro.sanz@live.u-tad.com>
2019-03-19 10:51:32 +0100

    finished 12th video, adding and removing elements from a collection

--- js/Models/Collections/AddingAndRemoving.js
+++ js/Models/Collections/AddingAndRemoving.js
@@ -9,8 +9,19 @@ let StudentCollection = Backbone.Collection.extend({
 });
 
 $(document).ready(function () {
-    let pepe = new StudentModel();
-    let luis = new StudentModel();
+    let pepe = new StudentModel({name: 'Pepe', id: 0});
+    let luis = new StudentModel({name: 'Luis', id: 1});
     let studentCollection = new StudentCollection([pepe, luis]);
-    console.log('cstudentCollection size: ' + studentCollection.size())
+    console.log('cstudentCollection size: ' + studentCollection.size());
+    displayCollectionContent("students", studentCollection);
+    let sara = new StudentModel({name: 'Sara', id: 2});
+    studentCollection.add([sara]);
+    displayCollectionContent("students and sara", studentCollection);
+    studentCollection.remove(pepe);
+    displayCollectionContent("students without pepe", studentCollection);
+
 });
+
+function displayCollectionContent(string, collection) {
+    console.log(string + " " + JSON.stringify(collection.toJSON()));
+}
